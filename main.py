c291cb770a82e5d0a0fd5b8a2cea9f5d3dbd4b27
life ⚡ <life@shellz.wtf>
2023-01-30 23:08:34 -0800

    YOLO

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import cv2
 import time
 import torch
-from transformers import YolosFeatureExtractor, YolosForObjectDetection
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from torchvision.transforms import ToPILImage
+import yolov5
+# medium model
+# https://github.com/ultralytics/yolov5/releases/download/v6.0/yolov5m.pt
+
+def yolov5_inference(
+    image = None,
+    model_path = None,
+    image_size = 640,
+    conf_threshold = 0.25,
+    iou_threshold = 0.45,
+):
+    """
+    YOLOv5 inference function
+    Args:
+        image: Input image
+        model_path: Path to the model
+        image_size: Image size
+        conf_threshold: Confidence threshold
+        iou_threshold: IOU threshold
+    Returns:
+        Rendered image
+    """
+    model = yolov5.load(model_path, device="cpu")
+    model.conf = conf_threshold
+    model.iou = iou_threshold
+    results = model([image], size=image_size)
+    return results.render()[0]
+        
 
 #Check for GPU Support 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -11,26 +42,13 @@ webCam = cv2.VideoCapture(0)
 
 #Extract image features
 
-feature_extractor = YolosFeatureExtractor.from_pretrained("hustvl/yolos-base")
-#YOLO Model Small
-with torch.no_grad():
-    model = YolosForObjectDetection.from_pretrained("hustvl/yolos-base")
-    model.to(device)
 while True:
-    print('\x1bc')
+    # print('\x1bc')
     return_value,image = webCam.read()
-    inputs = feature_extractor(images=image, return_tensors="pt")
-    with torch.no_grad():
-        inputs.to(device)
-        outputs = model(**inputs)
-    logits = outputs.logits
-    bboxes = outputs.pred_boxes
-    pred_labels = logits.argmax(dim=2)
-    
-    print("Detected:")
-    for label in pred_labels[0]:    
-        if label.item() == 91:
-            continue
-        print(model.config.id2label[label.item()])
-    time.sleep(5)
+    annotated_image = yolov5_inference(image, "./yolov5s.pt")
+    cv2.imshow('webcam',annotated_image)
+    if cv2.waitKey(1)& 0xFF == ord('q'):
+        break
+    time.sleep(0.01)
+  
 webCam.release()
